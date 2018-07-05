634d381b41fbf05e88b438953de371c85c866577
Adam Retter <adam.retter@googlemail.com>
2018-07-05 16:51:14 +0530

    Further utilities for working with Java's Optional

--- src/main/java/com/evolvedbinary/j8fu/OptionalUtil.java
+++ src/main/java/com/evolvedbinary/j8fu/OptionalUtil.java
@@ -26,8 +26,14 @@
  */
 package com.evolvedbinary.j8fu;
 
-import java.util.Optional;
+import com.evolvedbinary.j8fu.tuple.*;
+
+import java.util.*;
 import java.util.function.Supplier;
+
+import static com.evolvedbinary.j8fu.Either.Left;
+import static com.evolvedbinary.j8fu.Either.Right;
+import static com.evolvedbinary.j8fu.tuple.Tuple.Tuple;
 
 /**
  * Functional utility methods that are missing from {@link java.util.Optional}
@@ -37,7 +43,7 @@ import java.util.function.Supplier;
 public class OptionalUtil {
 
     /**
-     * Return the left Optional if present, else the right Optional
+     * Return the left Optional if present, else the right Optional.
      *
      * @param left The left of the disjunction
      * @param right The right of the disjunction
@@ -55,7 +61,7 @@ public class OptionalUtil {
     }
 
     /**
-     * A lazy version of {@link OptionalUtil#or(Optional, Optional)}
+     * A lazy version of {@link #or(Optional, Optional)}.
      *
      * @param left The left of the disjunction
      * @param right A lazily evaluated supplier of Optional for the right of the disjunction,
@@ -71,5 +77,198 @@ public class OptionalUtil {
         } else {
             return right.get();
         }
+    }
+
+    /**
+     * Constructs a Tuple if both Optionals are present.
+     *
+     * @param _1 The first Optional
+     * @param _2 The second Optional
+     *
+     * @param <T1> The type of the first value
+     * @param <T2> The type of the second value
+     *
+     * @return An Optional Tuple, or an empty Optional.
+     */
+    public static <T1, T2> Optional<Tuple2<T1, T2>> and(final Optional<T1> _1, final Optional<T2> _2) {
+        return _1.flatMap(t1 -> _2.map(t2 -> Tuple(t1, t2)));
+    }
+
+    /**
+     * Constructs a Tuple if all three Optionals are present.
+     *
+     * @param _1 The first Optional
+     * @param _2 The second Optional
+     * @param _3 The third Optional
+     *
+     * @param <T1> The type of the first value
+     * @param <T2> The type of the second value
+     * @param <T3> The type of the third value
+     *
+     * @return An Optional Tuple, or an empty Optional.
+     */
+    public static <T1, T2, T3> Optional<Tuple3<T1, T2, T3>> and(final Optional<T1> _1, final Optional<T2> _2, final Optional<T3> _3) {
+        return _1.flatMap(t1 -> _2.flatMap(t2 -> _3.map(t3 -> Tuple(t1, t2, t3))));
+    }
+
+    /**
+     * Constructs a Tuple if all four Optionals are present.
+     *
+     * @param _1 The first Optional
+     * @param _2 The second Optional
+     * @param _3 The third Optional
+     * @param _4 The fourth Optional
+     *
+     * @param <T1> The type of the first value
+     * @param <T2> The type of the second value
+     * @param <T3> The type of the third value
+     * @param <T4> The type of the fourth value
+     *
+     * @return An Optional Tuple, or an empty Optional.
+     */
+    public static <T1, T2, T3, T4> Optional<Tuple4<T1, T2, T3, T4>> and(final Optional<T1> _1, final Optional<T2> _2, final Optional<T3> _3, final Optional<T4> _4) {
+        return _1.flatMap(t1 -> _2.flatMap(t2 -> _3.flatMap(t3 -> _4.map(t4 -> Tuple(t1, t2, t3, t4)))));
+    }
+
+    /**
+     * Constructs a Tuple if all five Optionals are present.
+     *
+     * @param _1 The first Optional
+     * @param _2 The second Optional
+     * @param _3 The third Optional
+     * @param _4 The fourth Optional
+     * @param _5 The fifth Optional
+     *
+     * @param <T1> The type of the first value
+     * @param <T2> The type of the second value
+     * @param <T3> The type of the third value
+     * @param <T4> The type of the fourth value
+     * @param <T5> The type of the fifth value
+     *
+     * @return An Optional Tuple, or an empty Optional.
+     */
+    public static <T1, T2, T3, T4, T5> Optional<Tuple5<T1, T2, T3, T4, T5>> and(final Optional<T1> _1, final Optional<T2> _2, final Optional<T3> _3, final Optional<T4> _4, final Optional<T5> _5) {
+        return _1.flatMap(t1 -> _2.flatMap(t2 -> _3.flatMap(t3 -> _4.flatMap(t4 -> _5.map(t5 -> Tuple(t1, t2, t3, t4, t5))))));
+    }
+
+    /**
+     * Constructs a Tuple if all six Optionals are present.
+     *
+     * @param _1 The first Optional
+     * @param _2 The second Optional
+     * @param _3 The third Optional
+     * @param _4 The fourth Optional
+     * @param _5 The fifth Optional
+     * @param _6 The sixth Optional
+     *
+     * @param <T1> The type of the first value
+     * @param <T2> The type of the second value
+     * @param <T3> The type of the third value
+     * @param <T4> The type of the fourth value
+     * @param <T5> The type of the fifth value
+     * @param <T6> The type of the sixth value
+     *
+     * @return An Optional Tuple, or an empty Optional.
+     */
+    public static <T1, T2, T3, T4, T5, T6> Optional<Tuple6<T1, T2, T3, T4, T5, T6>> and(final Optional<T1> _1, final Optional<T2> _2, final Optional<T3> _3, final Optional<T4> _4, final Optional<T5> _5, final Optional<T6> _6) {
+        return _1.flatMap(t1 -> _2.flatMap(t2 -> _3.flatMap(t3 -> _4.flatMap(t4 -> _5.flatMap(t5 -> _6.map(t6 -> Tuple(t1, t2, t3, t4, t5, t6)))))));
+    }
+
+    /**
+     * If the optional is present, then its value is returned as the
+     * Left of a disjunction, otherwise the right value is returned.
+     *
+     * @param left The optional which could form the left
+     * @param right The value for the right, if the optional is empty
+     *
+     * @return The disjunction.
+     */
+    private static <L, R> Either<L,R> toLeft(final Optional<L> left, final R right) {
+        if (left.isPresent()) {
+            return Left(left.get());
+        } else {
+            return Right(right);
+        }
+    }
+
+
+    /**
+     * A lazy version of {@link #toLeft(Optional, Object)}.
+     *
+     * @param left The optional which could form the left
+     * @param right The value for the right, if the optional is empty
+     *
+     * @return The disjunction.
+     */
+    private static <L, R> Either<L,R> toLeft(final Optional<L> left, final Supplier<R> right) {
+        if (left.isPresent()) {
+            return Left(left.get());
+        } else {
+            return Right(right.get());
+        }
+    }
+
+    /**
+     * If the optional is present, them its value is returned as the
+     * Right of a disjunction, otherwise the left value is returned.
+     *
+     * @param left The value for the left, if the optional is empty
+     * @param right The optional which could form the right
+     *
+     * @return The disjunction.
+     */
+    private static <L, R> Either<L,R> toRight(final L left, final Optional<R> right) {
+        if (!right.isPresent()) {
+            return Left(left);
+        } else {
+            return Right(right.get());
+        }
+    }
+
+    /**
+     * A lazy version of {@link #toRight(Object, Optional)}.
+     *
+     * @param left The value for the left, if the optional is empty
+     * @param right The optional which could form the right
+     *
+     * @return The disjunction.
+     */
+    private static <L, R> Either<L,R> toRight(final Supplier<L> left, final Optional<R> right) {
+        if (!right.isPresent()) {
+            return Left(left.get());
+        } else {
+            return Right(right.get());
+        }
+    }
+
+    /**
+     * Creates a List from an Optional.
+     *
+     * The list will contain zero of one items.
+     *
+     * @param optional The optional to construct the list from.
+     *
+     * @return the list.
+     */
+    public static <T> List<T> toList(final Optional<T> optional) {
+        return optional.map(value -> {
+            final List<T> list = new ArrayList<>();
+            list.add(value);
+            return list;
+        }).orElseGet(() -> new ArrayList<>());
+    }
+
+    /**
+     * Creates an Immutable List from an Optional.
+     *
+     * The list will contain zero of one items.
+     *
+     * @param optional The optional to construct the list from.
+     *
+     * @return the list.
+     */
+    public static <T> List<T> toImmutableList(final Optional<T> optional) {
+        return optional.map(Arrays::asList)
+            .orElse(Collections.emptyList());
     }
 }
