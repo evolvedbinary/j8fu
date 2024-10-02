f8ae29a51c5fede4103deee36fdde97491d3454e
Adam Retter <adam.retter@googlemail.com>
2024-10-02 13:11:43 +0200

    Add some Quad Consumers

---
+++ src/main/java/com/evolvedbinary/j8fu/function/QuadConsumerE.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright © 2016, Evolved Binary Ltd. <tech@evolvedbinary.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.evolvedbinary.j8fu.function;
+
+import java.util.Objects;
+
+/**
+ * Similar to {@link BiConsumerE} but
+ * accepts four arguments
+ *
+ * @param <T> the type of the first input to the operation
+ * @param <U> the type of the second input to the operation
+ * @param <V> the type of the third input to the operation
+ * @param <W> the type of the fourth input to the operation
+ * @param <E> Function throws exception type
+ *
+ * @author <a href="mailto:adam@evolvedbinary.com">Adam Retter</a>
+ */
+@FunctionalInterface
+public interface QuadConsumerE<T, U, V, W, E extends Throwable> {
+
+    /**
+     * Performs this operation on the given arguments.
+     *
+     * @param t the first input argument
+     * @param u the second input argument
+     * @param v the third input argument
+     * @param w the fourth input argument
+     *
+     * @throws E An exception of type {@code E}
+     */
+    void accept(T t, U u, V v, W w) throws E;
+
+    /**
+     * Returns a composed {@code QuadConsumerE} that performs, in sequence, this
+     * operation followed by the {@code after} operation. If performing either
+     * operation throws an exception, it is relayed to the caller of the
+     * composed operation.  If performing this operation throws an exception,
+     * the {@code after} operation will not be performed.
+     *
+     * @param after the operation to perform after this operation
+     * @return a composed {@code QuadConsumerE} that performs in sequence this
+     * operation followed by the {@code after} operation
+     * @throws NullPointerException if {@code after} is null
+     */
+    default QuadConsumerE<T, U, V, W, E> andThen(final QuadConsumerE<? super T, ? super U, ? super V, ? super W, ? extends E> after) {
+        Objects.requireNonNull(after);
+
+        return (t, u, v, w) -> {
+            accept(t, u, v, w);
+            after.accept(t, u, v, w);
+        };
+    }
+
+    /**
+     * Returns a composed {@code QuadConsumerE} that performs, in sequence, this
+     * operation followed by the {@code after} operation. If performing either
+     * operation throws an exception, it is relayed to the caller of the
+     * composed operation.  If performing this operation throws an exception,
+     * the {@code after} operation will not be performed.
+     *
+     * @param after the operation to perform after this operation
+     * @return a composed {@code QuadConsumerE} that performs in sequence this
+     * operation followed by the {@code after} operation
+     * @throws NullPointerException if {@code after} is null
+     */
+    default QuadConsumerE<T, U, V, W, E> andThen(final QuadConsumer<? super T, ? super U, ? super V, ? super W> after) {
+        Objects.requireNonNull(after);
+
+        return (t, u, v, w) -> {
+            accept(t, u, v, w);
+            after.accept(t, u, v, w);
+        };
+    }
+}
